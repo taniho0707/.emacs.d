787ce66abcceb87bb6cdf5e405d2bbd86e27e66a
taniho_0707 <taniho_0707@yahoo.co.jp>
2016-06-03 22:05:05 +0900

    Add Haskell mode

--- lisp/init-additional.el
+++ lisp/init-additional.el
@@ -88,6 +88,21 @@
 ;; perl-completion
 ;; (require 'perl-completion)
 
+;; Haskell mode
+;; http://futurismo.biz/archives/2662
+(autoload 'haskell-mode "haskell-mode" nil t)
+(autoload 'haskell-cabal "haskell-cabal" nil t)
+ 
+(add-to-list 'auto-mode-alist '("\\.hs$" . haskell-mode))
+(add-to-list 'auto-mode-alist '("\\.lhs$" . literate-haskell-mode))
+(add-to-list 'auto-mode-alist '("\\.cabal\\'" . haskell-cabal-mode))
+
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'font-lock-mode)
+(add-hook 'haskell-mode-hook 'imenu-add-menubar-index)
+
+
 ;; emmet-mode
 (require 'emmet-mode)
 (add-hook 'sgml-mode-hook 'emmet-mode)
